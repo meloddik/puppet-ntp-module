63e19792a1ea7b6db44d2e784627454c3a7f492b
meloddik <jboddie29@gmail.com>
2025-01-05 00:03:17 -0500

    Create custom ntp class

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+class brakebills_ntp {
+    ensure_packages('ntp')
+
+    file {'/etc/ntp.conf':
+        source  => 'puppet: /brakebills_ntp/ntp.conf',
+        notify  => Service['ntp'],
+        require => Package['ntp'],
+    }
+
+    service {'ntp':
+        ensure => running,
+        enable => true
+    }
+}
+
+
